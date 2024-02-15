737e2c5484dfc85d0d2c8dd89dd2b6015f9a08db
Davius <66792063+Xdavius@users.noreply.github.com>
2024-02-15 04:10:55 +0100

    Change all nvidia names

---
+++ vortex-repository/pacscript/packages/nvidia-driver-functionality-dkms-bundle/get-archlinux-patches.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://gitlab.archlinux.org/archlinux/packaging/packages/nvidia-utils/-/raw/main/nvidia-drivers-470.223.02-gpl-pfn_valid.patch
+wget https://gitlab.archlinux.org/archlinux/packaging/packages/nvidia-utils/-/raw/main/nvidia.rules
+wget https://gitlab.archlinux.org/archlinux/packaging/packages/nvidia-utils/-/raw/main/nvidia-drm-outputclass.conf
+wget https://gitlab.archlinux.org/archlinux/packaging/packages/nvidia-utils/-/raw/main/nvidia-utils.sysusers
