19b0a79ea95142328d0fe1a31f13b537f82caa39
Davius <66792063+Xdavius@users.noreply.github.com>
2024-02-23 14:24:54 +0100

    Update getsha.sh

--- vortex-repository/getsha.sh
+++ vortex-repository/getsha.sh
@@ -3,7 +3,13 @@ pkgver=$(curl --silent "https://api.github.com/repos/Frogging-Family/linux-tkg/r
     grep '"tag_name":' |
     sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-)
 
+url="full.tar.gz" 
+hash="$(curl -sL $url | sha256sum | cut -d " " -f 1)"
+
+echo "URL : $url
+HASH : $hash
+"
+
 # ONE LINE HASH
 
-url="full.tar.gz" ;\
-hash="$(curl -sL $url | sha256sum | cut -d " " -f 1)"
+url="full.tar.gz" ; curl -sL $url | sha256sum | cut -d " " -f 1
